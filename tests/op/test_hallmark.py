10de8a8bf61a21f2238ba43c8b8e1fe4fd4d74fc
Yimin Zheng <zym.zym1220@gmail.com>
2025-08-08 09:40:24 +0000

    add test

--- tests/op/test_hallmark.py
+++ tests/op/test_hallmark.py
@@ -9,3 +9,12 @@ def test_hallmark():
     cols = {"source", "target"}
     assert cols.issubset(hm.columns)
     assert not hm.duplicated(["source", "target"]).any()
+
+
+def test_hallmark_as_future():
+    future = dc.op.hallmark(as_future=True)
+    hm = future.result()
+    assert isinstance(hm, pd.DataFrame)
+    cols = {"source", "target"}
+    assert cols.issubset(hm.columns)
+    assert not hm.duplicated(["source", "target"]).any()
